7e4e458571f7520dec13a8aa576a23eab1943fa5
Chris M <webmaster@magnaws.com>
2017-05-24 09:27:06 -0400

    Initial Commit

---
+++ LGV_Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LGV_Timer
+//
+//  Created by Chris Marshall on 5/24/17.
+//  Copyright © 2017 Little Green Viper Software Development LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
