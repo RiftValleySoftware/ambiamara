bba78a580aac8252ee115acd9577d2c13d2e309e
Chris M <webmaster@magnaws.com>
2017-05-24 21:09:23 -0400

    Added a background gradient.

--- LGV_Timer/ViewControllers/LGV_Timer_TimerBaseViewController.swift
+++ LGV_Timer/ViewControllers/LGV_Timer_TimerBaseViewController.swift
@@ -15,14 +15,31 @@ import UIKit
 /**
  */
 @IBDesignable class LGV_Timer_TimerBaseViewController: UIViewController {
-    @IBInspectable var gradientTopColor: UIColor = UIColor.green {
+    @IBInspectable var gradientTopColor: UIColor = UIColor.black {
         didSet{
             self.view.setNeedsLayout()
         }
     }
-    @IBInspectable var gradientBottomColor: UIColor = UIColor.blue {
+    @IBInspectable var gradientBottomColor: UIColor = UIColor.darkGray {
         didSet{
             self.view.setNeedsLayout()
         }
+    }
+    
+    let gradientLayer = CAGradientLayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.gradientLayer.colors = [self.gradientTopColor.cgColor, self.gradientBottomColor.cgColor]
+        
+        self.gradientLayer.locations = [0.0, 1.0]
+        
+        self.view.layer.addSublayer(self.gradientLayer)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.gradientLayer.frame = self.view.bounds
     }
 }
