c3ccd70ca22bd15554d0ca736b7b222e2b51f6d5
Chris M <webmaster@magnaws.com>
2017-05-27 15:21:13 -0400

    I think the prefs are pretty much "fleshed out."

--- LGV_Timer/App Files/LGV_Timer_StaticPrefs.swift
+++ LGV_Timer/App Files/LGV_Timer_StaticPrefs.swift
@@ -8,6 +8,149 @@
 
 import Foundation
 
+// MARK: - Convenience Cast -
+/* ###################################################################################################################################### */
+extension Int {
+    /* ################################################################## */
+    /**
+     Casting operator for TimeTuple to an integer.
+     */
+    init(_ inTimeTuple: TimeTuple) {
+        self.init(inTimeTuple.intVal)
+    }
+}
+
+// MARK: - TimeTuple Class -
+/* ###################################################################################################################################### */
+/**
+ This is a class designed to behave like a tuple.
+ 
+ It holds the timer value as hours, minutes and seconds.
+ */
+class TimeTuple {
+    // MARK: - Private Instance Properties
+    /* ################################################################################################################################## */
+    private var _hours: Int     ///< The number of hours in this instance, from 0 - 23
+    private var _minutes: Int   ///< The number of minutes in this instance, from 0 - 59
+    private var _seconds: Int   ///< The number of seconds in this instance, from 0 - 59
+    
+    // MARK: - Internal Calculated Properties
+    /* ################################################################################################################################## */
+    /* ################################################################## */
+    /**
+     This sets/returns the value of this class, as a total sum of seconds (0 - 86399)
+     */
+    var intVal: Int {
+        get {
+            return min(86399, max(0, (self.hours * 3600) + (self.minutes * 60) + self.seconds))
+        }
+        
+        set {
+            let temp = min(86399, max(0, newValue))
+            
+            self.hours = max(0, Int(temp / 3600))
+            self.minutes = max(0, Int(temp / 60) - (self.hours * 60))
+            self.seconds = max(0, Int(temp) - ((self.hours * 3600) + (self.minutes * 60)))
+        }
+    }
+    
+    /* ################################################################## */
+    /**
+     Public accessor fo the hours.
+     */
+    var hours: Int {
+        get {
+            return self._hours
+        }
+        
+        set {
+            self._hours = min(23, max(0, newValue))
+        }
+    }
+    
+    /* ################################################################## */
+    /**
+     Public accessor fo the minutes.
+     */
+    var minutes: Int {
+        get {
+            return self._minutes
+        }
+        
+        set {
+            self._minutes = min(59, max(0, newValue))
+        }
+    }
+    
+    /* ################################################################## */
+    /**
+     Public accessor fo the seconds.
+     */
+    var seconds: Int {
+        get {
+            return self._seconds
+        }
+        
+        set {
+            self._seconds = min(59, max(0, newValue))
+        }
+    }
+    
+    /* ################################################################## */
+    /**
+     Returns the value of the instance as DateComponents, for easy use in date calculations.
+     */
+    var dateComponents: DateComponents {
+        get {
+            return DateComponents(calendar: nil, timeZone: nil, era: nil, year: nil, month: nil, day: nil, hour: self._hours, minute: self._minutes, second: self._seconds, nanosecond: nil, weekday: nil, weekdayOrdinal: nil, quarter: nil, weekOfMonth: nil, weekOfYear: nil, yearForWeekOfYear: nil)
+        }
+        
+        set {
+            self.intVal = 0
+            
+            if let hour = newValue.hour {
+                self.hours = Int(hour)
+            }
+            if let minute = newValue.minute {
+                self.minutes = Int(minute)
+            }
+            if let second = newValue.second {
+                self.seconds = Int(second)
+            }
+        }
+    }
+    
+    // MARK: - Initializers
+    /* ################################################################################################################################## */
+    /* ################################################################## */
+    /**
+     Initialize just like a tuple.
+     
+     :param: hours The number of hours, as an Int, from 0 - 23.
+     :param: minutes The number of minutes, as an Int, from 0 - 59.
+     :param: seconds The number of seconds, as an Int, from 0 - 59.
+     */
+    init(hours: Int, minutes: Int, seconds: Int) {
+        self._hours = max(0, min(23, hours))
+        self._minutes = max(0, min(59, minutes))
+        self._seconds = max(0, min(59, seconds))
+    }
+    
+    /* ################################################################## */
+    /**
+     Initialize from total seconds.
+     
+     :param: The number of seconds as an Int, from 0 - 86399.
+     */
+    init(_ inSeconds:Int) {
+        let temp = min(86399, max(0, inSeconds))
+        
+        self._hours = max(0, Int(temp / 3600))
+        self._minutes = max(0, Int(temp / 60) - (self._hours * 60))
+        self._seconds = max(0, Int(temp) - ((self._hours * 3600) + (self._minutes * 60)))
+    }
+}
+
 // MARK: - Prefs Class -
 /* ###################################################################################################################################### */
 /**
@@ -16,6 +159,16 @@ import Foundation
  type of prefs most users set in their "gear" screen.
  */
 class LGV_Timer_StaticPrefs {
+    /// This is the basic element that describes one timer.
+    typealias TimerSettingTuple = (
+        timeSet: Int,                   ///< This is the set (start) time for the countdown timer. It is an integer, with the number of seconds (0 - 86399)
+        timeSetPodiumWarn: Int,         ///< This is the number of seconds (0 - 86399) before the yellow light comes on in Podium Mode. If 0, then it is automatically calculated.
+        timeSetPodiumFinal: Int,        ///< This is the number of seconds (0 - 86399) before the red light comes on in Podium Mode. If 0, then it is automatically calculated.
+        displayMode: TimerDisplayMode,  ///< This is how the timer will display
+        keepsDeviceAwake: Bool,         ///< Detemines whether or not to keep the device awake while counting down.
+        colorTheme: Int                 ///< This is the 0-based index for the color theme.
+    )
+    
     // MARK: - Private Static Properties
     /* ################################################################################################################################## */
     /* ################################################################## */
@@ -41,7 +194,41 @@ class LGV_Timer_StaticPrefs {
         case KeepAwakeStopwatch = "KeepAwakeStopwatch"
         /** This will be a Boolean pref, indicating whether or not the stopwatch will display a lap counter. */
         case UseLapsStopwatch = "UseLapsStopwatch"
+        /** This will be an array of dictionaries, with a list of timers. */
+        case TimerList = "TimerList"
     }
+    
+    // MARK: - Internal Enums
+    /* ################################################################################################################################## */
+    /* ################################################################## */
+    /** These are the keys we use for our timer prefs dictionary. */
+    enum TimerPrefKeys: String {
+        case TimeSet            = "TimeSet"
+        case TimeSetPodiumWarn  = "TimeSetPodiumWarn"
+        case TimeSetPodiumFinal = "TimeSetPodiumFinal"
+        case DisplayMode        = "DisplayMode"
+        case KeepsDeviceAwake   = "KeepsDeviceAwake"
+        case ColorTheme         = "ColorTheme"
+    }
+    
+    /* ################################################################## */
+    /**
+     These are the three display modes we have for our countdown timer.
+     */
+    enum TimerDisplayMode: Int {
+        case Digital    = 0 ///< Display only digits.
+        case Podium     = 1 ///< Display only "podium lights."
+        case Dual       = 2 ///< Display both.
+    }
+    
+    // MARK: - Private Static Constants
+    /* ################################################################################################################################## */
+    /* ################################################################## */
+    /**
+     These are the thresholds that we apply to our timer when automatically determining the "traffic lights" for podium mode.
+     */
+    static let _podiumModeWarningThreshold: Float  = (6 / 36)
+    static let _podiumModeFinalThreshold: Float    = (3 / 36)
     
     // MARK: - Private Initializer
     /* ################################################################################################################################## */
@@ -78,6 +265,32 @@ class LGV_Timer_StaticPrefs {
      */
     private func _savePrefs() {
         UserDefaults.standard.set(self._loadedPrefs, forKey: type(of: self)._mainPrefsKey)
+    }
+    
+    // MARK: - Internal Class Methods
+    /* ################################################################################################################################## */
+    /* ################################################################## */
+    /**
+     This calculates the auto-calculation for the "warning," or "yellow traffic light" for the Podium Mode timer.
+     
+     :param: inTimerSet The value of the countdown timer.
+     
+     :returns: an Int, with the warning threshold.
+     */
+    class func calcPodiumModeWarningThresholdForTimerValue(_ inTimerSet: Int) -> Int {
+        return max(0, min(inTimerSet, Int(ceil(Float(inTimerSet) * self._podiumModeWarningThreshold))))
+    }
+    
+    /* ################################################################## */
+    /**
+     This calculates the auto-calculation for the "final," or "red traffic light" for the Podium Mode timer.
+     
+     :param: inTimerSet The value of the countdown timer.
+     
+     :returns: an Int, with the final threshold.
+     */
+    class func calcPodiumModeFinalThresholdForTimerValue(_ inTimerSet: Int) -> Int {
+        return max(0, min(calcPodiumModeWarningThresholdForTimerValue(inTimerSet), Int(ceil(Float(inTimerSet) * self._podiumModeFinalThreshold))))
     }
     
     // MARK: - Class Static Properties
@@ -137,6 +350,7 @@ class LGV_Timer_StaticPrefs {
     /* ################################################################################################################################## */
     /* ################################################################## */
     /**
+     This sets/returns whether or not the clock keeps the device awake while timing.
      */
     var clockKeepsDeviceAwake: Bool {
         get {
@@ -161,6 +375,7 @@ class LGV_Timer_StaticPrefs {
     
     /* ################################################################## */
     /**
+     This sets/returns whether or not the stopwatch keeps the device awake while timing.
      */
     var stopwatchKeepsDeviceAwake: Bool {
         get {
@@ -185,6 +400,7 @@ class LGV_Timer_StaticPrefs {
     
     /* ################################################################## */
     /**
+     This sets/returns whether or not the stopwatch tracks laps.
      */
     var stopwatchTracksLaps: Bool {
         get {
@@ -203,6 +419,117 @@ class LGV_Timer_StaticPrefs {
             if self._loadPrefs() {
                 let savedVal = NSNumber(value: newValue)
                 self._loadedPrefs.setObject(savedVal, forKey: PrefsKeys.UseLapsStopwatch.rawValue as NSCopying)
+            }
+        }
+    }
+    
+    /* ################################################################## */
+    /**
+     This sets/returns a list of timers. We must have at least one timer.
+     */
+    var timers:[TimerSettingTuple] {
+        get {
+            var ret: [TimerSettingTuple] = []
+            let defaultValue: TimerSettingTuple = (timeSet: 0, timeSetPodiumWarn: 0, timeSetPodiumFinal: 0, displayMode: .Digital, keepsDeviceAwake: true, colorTheme: 0)
+            
+            if self._loadPrefs() {
+                if let temp = self._loadedPrefs.object(forKey: PrefsKeys.TimerList.rawValue) as? NSArray {
+                    for index in 0..<temp.count {
+                        if let arrayElement = temp[index] as? NSDictionary {
+                            var temp: TimerSettingTuple = defaultValue
+                            print("\(arrayElement)")
+                            if let timeSet = arrayElement.object(forKey: TimerPrefKeys.TimeSet.rawValue as NSString) as? NSNumber {
+                                temp.timeSet = min(86399, max(0, timeSet.intValue))
+                            }
+                            
+                            if let timeSetPodiumWarn = arrayElement.object(forKey: TimerPrefKeys.TimeSetPodiumWarn.rawValue as NSString) as? NSNumber {
+                                temp.timeSetPodiumWarn = max(0, timeSetPodiumWarn.intValue)
+                            }
+                            
+                            if let timeSetPodiumFinal = arrayElement.object(forKey: TimerPrefKeys.TimeSetPodiumFinal.rawValue as NSString) as? NSNumber {
+                                temp.timeSetPodiumFinal = max(0, timeSetPodiumFinal.intValue)
+                            }
+                            
+                            if let displayMode = arrayElement.object(forKey: TimerPrefKeys.DisplayMode.rawValue as NSString) as? NSNumber {
+                                if let dMode = TimerDisplayMode(rawValue: displayMode.intValue) {
+                                    temp.displayMode = dMode
+                                }
+                            }
+                            
+                            if let keepsDeviceAwake = arrayElement.object(forKey: TimerPrefKeys.KeepsDeviceAwake.rawValue as NSString) as? NSNumber {
+                                temp.keepsDeviceAwake = keepsDeviceAwake.boolValue
+                            }
+
+                            if let colorTheme = arrayElement.object(forKey: TimerPrefKeys.ColorTheme.rawValue as NSString) as? NSNumber {
+                                temp.colorTheme = colorTheme.intValue
+                            }
+                            
+                            // If either of these is zero, then we need to calculate the thresholds.
+                            if (0 == temp.timeSetPodiumWarn) || (0 == temp.timeSetPodiumFinal) {
+                                if 0 < temp.timeSet {
+                                    temp.timeSetPodiumWarn = type(of: self).calcPodiumModeWarningThresholdForTimerValue(temp.timeSet)
+                                    temp.timeSetPodiumFinal = type(of: self).calcPodiumModeFinalThresholdForTimerValue(temp.timeSet)
+                                } else {
+                                    temp.timeSetPodiumWarn = 0
+                                    temp.timeSetPodiumFinal = 0
+                                }
+                            }
+                            
+                            ret.append(temp)
+                        } else {
+                            ret.append(defaultValue)
+                        }
+                    }
+                } else {
+                    ret.append(defaultValue)
+                }
+            } else {
+                ret.append(defaultValue)
+            }
+            
+            return ret
+        }
+        
+        set {
+            if self._loadPrefs() {
+                let tempSetting:NSMutableArray = []
+                
+                for timer in newValue {
+                    let timerInstance = NSDictionary()
+                    
+                    let timeSetKey = TimerPrefKeys.TimeSet.rawValue
+                    let timeSetValue = NSNumber(value: min(86399, max(0, timer.timeSet)))
+                    timerInstance.setValue(timeSetValue, forKey: timeSetKey)
+                    
+                    let timeSetPodiumWarnKey = TimerPrefKeys.TimeSetPodiumWarn.rawValue
+                    let timeSetPodiumWarnValue = NSNumber(value: min(timeSetValue.intValue, max(0, timer.timeSetPodiumWarn)))
+                    timerInstance.setValue(timeSetPodiumWarnValue, forKey: timeSetPodiumWarnKey)
+                    
+                    let timeSetPodiumFinalKey = TimerPrefKeys.TimeSetPodiumFinal.rawValue
+                    let timeSetPodiumFinalValue = NSNumber(value: min(timeSetPodiumWarnValue.intValue, max(0, timer.timeSetPodiumWarn)))
+                    timerInstance.setValue(timeSetPodiumFinalValue, forKey: timeSetPodiumFinalKey)
+                    
+                    let displayModeKey = TimerPrefKeys.DisplayMode.rawValue
+                    let displayModeValue = NSNumber(value: timer.displayMode.rawValue)
+                    timerInstance.setValue(displayModeValue, forKey: displayModeKey)
+                    
+                    let keepsAwakeKey = TimerPrefKeys.KeepsDeviceAwake.rawValue
+                    let keepsAwakeValue = NSNumber(value: timer.keepsDeviceAwake)
+                    timerInstance.setValue(keepsAwakeValue, forKey: keepsAwakeKey)
+                    
+                    let colorThemeKey = TimerPrefKeys.ColorTheme.rawValue
+                    let colorThemeValue = NSNumber(value: timer.colorTheme)
+                    timerInstance.setValue(colorThemeValue, forKey: colorThemeKey)
+                    
+                    tempSetting.add(timerInstance)
+                }
+                
+                // We're not allowed to have zero timers.
+                if 0 == tempSetting.count {
+                    tempSetting.add(TimerSettingTuple(timeSet: 0, timeSetPodiumWarn: 0, timeSetPodiumFinal: 0, displayMode: .Digital, keepsDeviceAwake: true, colorTheme: 0))
+                }
+                
+                self._loadedPrefs.setObject(tempSetting, forKey: PrefsKeys.TimerList.rawValue as NSCopying)
             }
         }
     }
