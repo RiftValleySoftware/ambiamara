0e7b81d144f973a6403cde9d62fd6b046d084958
Chris Marshall <chris@riftvalleysoftware.com>
2019-04-13 14:01:09 -0400

    Just made the utility code a bit better.

--- Shared/Timer_SharedDeclarations.swift
+++ Shared/Timer_SharedDeclarations.swift
@@ -116,36 +116,10 @@ extension UIView {
         self.addSubview(inSubView)
         
         inSubView.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.addConstraints([
-            NSLayoutConstraint(item: inSubView,
-                               attribute: .top,
-                               relatedBy: .equal,
-                               toItem: self,
-                               attribute: .top,
-                               multiplier: 1.0,
-                               constant: 0),
-            NSLayoutConstraint(item: inSubView,
-                               attribute: .left,
-                               relatedBy: .equal,
-                               toItem: self,
-                               attribute: .left,
-                               multiplier: 1.0,
-                               constant: 0),
-            NSLayoutConstraint(item: inSubView,
-                               attribute: .bottom,
-                               relatedBy: .equal,
-                               toItem: self,
-                               attribute: .bottom,
-                               multiplier: 1.0,
-                               constant: 0),
-            NSLayoutConstraint(item: inSubView,
-                               attribute: .right,
-                               relatedBy: .equal,
-                               toItem: self,
-                               attribute: .right,
-                               multiplier: 1.0,
-                               constant: 0)])
+        inSubView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
+        inSubView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0).isActive = true
+        inSubView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0).isActive = true
+        inSubView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
     }
     
     /* ################################################################## */
