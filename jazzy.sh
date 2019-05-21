12e090a216fe30e29b9e60866363156139f2f029
Chris Marshall <chris@riftvalleysoftware.com>
2019-05-21 07:03:27 -0400

    Tweaked the Jazzy file a bit.

--- jazzy.sh
+++ jazzy.sh
@@ -4,14 +4,15 @@ MY_SCRIPT_PATH=`dirname "${BASH_SOURCE[0]}"`
 cd "${MY_SCRIPT_PATH}"
 rm -drf docs
 jazzy   --github_url https://github.com/RiftValleySoftware/ambiamara\
-        --readme ./README.md --theme fullwidth\
+        --readme ./README.md \
+        --theme fullwidth \
         --author The\ Great\ Rift\ Valley\ Software\ Company\
         --author_url https://riftvalleysoftware.com\
         --module AmbiaMara \
         --min-acl private \
         --exclude=./*/Swipe*,./*/*/TheGreatRiftValleyDrawing.swift,./*/TimerSoundModeButton.swift \
-        -x CODE_SIGNING_ALLOWED=NO \
-        --copyright [©2019\ The\ Great\ Rift\ Valley\ Software\ Company]\(https://riftvalleysoftware.com\)
+        --copyright [©2019\ The\ Great\ Rift\ Valley\ Software\ Company]\(https://riftvalleysoftware.com\) \
+        -x CODE_SIGNING_ALLOWED=NO
 cp icon.png docs/icon.png
 cp img/*.* docs/img/
 cd "${CWD}"
