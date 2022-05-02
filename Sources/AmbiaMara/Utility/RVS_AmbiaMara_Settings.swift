23c45ba5dcb8a422132512837eabf1546355b9f5
Chris Marshall <chris@riftvalleysoftware.com>
2022-05-02 12:08:03 -0400

    Redid the licensing header.

--- Sources/AmbiaMara/Utility/RVS_AmbiaMara_Settings.swift
+++ Sources/AmbiaMara/Utility/RVS_AmbiaMara_Settings.swift
@@ -1,7 +1,7 @@
 /*
  © Copyright 2018-2022, The Great Rift Valley Software Company. All rights reserved.
  
- This code is proprietary and confidential code,
+ This code is proprietary code, and is not licensed for reuse.
  It is NOT to be reused or combined into any application,
  unless done so, specifically under written license from The Great Rift Valley Software Company.
  
