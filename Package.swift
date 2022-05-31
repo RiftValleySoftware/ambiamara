f039db634b8cc2fabc56fb139523fd75f9006dd8
Chris Marshall <chris@riftvalleysoftware.com>
2022-05-31 17:05:28 -0400

    Updated the SPM modules.

--- Package.swift
+++ Package.swift
@@ -26,27 +26,27 @@ let package = Package(
     dependencies: [
         .package(name: "RVS_Generic_Swift_Toolbox",
                  url: "git@github.com:RiftValleySoftware/RVS_Generic_Swift_Toolbox.git",
-                 from: "1.7.2"),
+                 from: "1.8.1"),
         .package(name: "RVS_BasicGCDTimer",
                  url: "git@github.com:RiftValleySoftware/RVS_BasicGCDTimer.git",
-                 from: "1.4.2"),
+                 from: "1.5.0"),
         .package(name: "RVS_PersistentPrefs",
                  url: "git@github.com:RiftValleySoftware/RVS_PersistentPrefs.git",
-                 from: "1.2.8"),
+                 from: "1.3.1"),
         .package(name: "RVS_RetroLEDDisplay",
                  url: "git@github.com:RiftValleySoftware/RVS_RetroLEDDisplay.git",
-                 from: "1.2.0")
+                 from: "1.4.1")
     ],
     targets: [
         .target(name: "AmbiaMara",
                 dependencies: [
-                    .product(name: "RVS-Generic-Swift-Toolbox",
+                    .product(name: "RVS_Generic_Swift_Toolbox",
                              package: "RVS_Generic_Swift_Toolbox"),
-                    .product(name: "RVS-BasicGCDTimer",
+                    .product(name: "RVS_BasicGCDTimer",
                              package: "RVS_BasicGCDTimer"),
-                    .product(name: "RVS-Persistent-Prefs",
+                    .product(name: "RVS_PersistentPrefs",
                              package: "RVS_PersistentPrefs"),
-                    .product(name: "RVS-RetroLEDDisplay",
+                    .product(name: "RVS_RetroLEDDisplay",
                              package: "RVS_RetroLEDDisplay")
                     ])
     ]
