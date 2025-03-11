ba1ac9fb2272b0266a58270815cfcb7965ead740
Chris Marshall <chris@riftvalleysoftware.com>
2025-03-11 17:54:21 -0400

    Continuing work on the selected timer screen. Added displays of the warn and final times.

--- Sources/WatchApp/Rift_Valley_Timer_Watch_App_TimerContentView.swift
+++ Sources/WatchApp/Rift_Valley_Timer_Watch_App_TimerContentView.swift
@@ -20,27 +20,59 @@ import RVS_Generic_Swift_Toolbox
 struct Rift_Valley_Timer_Watch_App_TimerContentView: View {
     /* ################################################################## */
     /**
+     This is the timer instance, associated with this screen.
     */
     @State var timer: RVS_AmbiaMara_Settings.TimerSettings
 
     /* ################################################################## */
     /**
+     This screen is only shown for the selected timer (which is selected upon entering the screen).
     */
     @Binding var selectedTimerIndex: Int
 
     /* ################################################################## */
     /**
+     It's a fairly basic VStack, with the timer start
     */
     var body: some View {
         let hour = (Int(timer.startTime) / 60) / 60
         let minute = (Int(timer.startTime) / 60) - (hour * 60)
         let second = Int(timer.startTime) - ((hour * 60) * 60) - ((minute * 60))
-        let startTimeString = String(format: "%02d:%02d:%02d", hour, minute, second)
+        let timeString = String(format: "%02d:%02d:%02d", hour, minute, second)
         VStack {
-            Text(startTimeString)
+            Text(timeString)
                 .frame(maxWidth: .infinity, alignment: .center)
                 .font(Font.custom("Let's go Digital Regular", size: 50))
+                .foregroundColor(Color("Start-Color"))
                 .onAppear { selectedTimerIndex = timer.index }
+            
+            if ((0 < timer.warnTime) && (timer.startTime > timer.warnTime)) || ((0 < timer.finalTime) && (timer.startTime > timer.finalTime)) {
+                HStack {
+                    if 0 < timer.warnTime,
+                       timer.startTime > timer.warnTime {
+                        let hour = (Int(timer.warnTime) / 60) / 60
+                        let minute = (Int(timer.warnTime) / 60) - (hour * 60)
+                        let second = Int(timer.warnTime) - ((hour * 60) * 60) - ((minute * 60))
+                        let timeString = String(format: "%02d:%02d:%02d", hour, minute, second)
+                        Text(timeString)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .font(Font.custom("Let's go Digital Regular", size: 20))
+                            .foregroundColor(Color("Warn-Color"))
+                    }
+                    
+                    if 0 < timer.finalTime,
+                       timer.startTime > timer.finalTime {
+                        let hour = (Int(timer.finalTime) / 60) / 60
+                        let minute = (Int(timer.finalTime) / 60) - (hour * 60)
+                        let second = Int(timer.finalTime) - ((hour * 60) * 60) - ((minute * 60))
+                        let timeString = String(format: "%02d:%02d:%02d", hour, minute, second)
+                        Text(timeString)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .font(Font.custom("Let's go Digital Regular", size: 20))
+                            .foregroundColor(Color("Final-Color"))
+                    }
+                }
+            }
         }
         .navigationTitle(Text(String(format: "SLUG-TIMER-FORMAT".localizedVariant, timer.index + 1)))
     }
