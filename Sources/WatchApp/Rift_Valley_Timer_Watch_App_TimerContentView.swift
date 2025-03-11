8e7c8dcc001b2113a6f677a78d3bf8fd2d591d79
Chris Marshall <chris@riftvalleysoftware.com>
2025-03-11 14:40:33 -0400

    Starting work on the individual timer screen.

--- Sources/WatchApp/Rift_Valley_Timer_Watch_App_TimerContentView.swift
+++ Sources/WatchApp/Rift_Valley_Timer_Watch_App_TimerContentView.swift
@@ -9,6 +9,7 @@
  */
 
 import SwiftUI
+import RVS_Generic_Swift_Toolbox
 
 /* ###################################################################################################################################### */
 // MARK: - Main Watch Content View -
@@ -31,9 +32,16 @@ struct Rift_Valley_Timer_Watch_App_TimerContentView: View {
     /**
     */
     var body: some View {
-        Text("HOWDY!")
-            .onAppear {
-                selectedTimerIndex = timer.index
-            }
+        let hour = (Int(timer.startTime) / 60) / 60
+        let minute = (Int(timer.startTime) / 60) - (hour * 60)
+        let second = Int(timer.startTime) - ((hour * 60) * 60) - ((minute * 60))
+        let startTimeString = String(format: "%02d:%02d:%02d", hour, minute, second)
+        VStack {
+            Text(startTimeString)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .font(Font.custom("Let's go Digital Regular", size: 50))
+                .onAppear { selectedTimerIndex = timer.index }
+        }
+        .navigationTitle(Text(String(format: "SLUG-TIMER-FORMAT".localizedVariant, timer.index + 1)))
     }
 }
