3ed4aa67be3384027c07bf5d17c9b5799827b9e1
Chris Marshall <chris@riftvalleysoftware.com>
2025-04-15 12:29:00 -0400

    OK. A simple instantiation test is done.

--- Tests/TimerModelTests/TimerModelTests.swift
+++ Tests/TimerModelTests/TimerModelTests.swift
@@ -35,12 +35,29 @@ class TimerModelTests: XCTestCase {
         print("TimerModelTests.testCreateSimple (START)")
         let timerModel = TimerModel()
         let timer = timerModel.createNewTimer(at: IndexPath(item: 0, section: 0))
+        timer.startingTimeInSeconds = 10
+        timer.warningTimeInSeconds = 5
+        timer.finalTimeInSeconds = 2
         XCTAssertFalse(timerModel.isEmpty)
         XCTAssertEqual(timerModel.count, 1)
+        XCTAssertEqual(timerModel[0].count, 1)
+        XCTAssertEqual(timerModel.allTimers.first, timer)
+        XCTAssertEqual(timerModel[0].allTimers.first, timer)
         XCTAssertEqual(timerModel[0][0], timer)
         XCTAssertEqual(timerModel[0], timer.group)
         XCTAssertEqual(timerModel, timer.model)
         XCTAssertEqual(timerModel.allTimers.first, timer)
+        XCTAssertEqual(timerModel.allTimers.first?.startingTimeInSeconds, timer.startingTimeInSeconds)
+        XCTAssertEqual(timerModel.allTimers.first?.startingTimeInSeconds, 10)
+        XCTAssertEqual(timerModel[0].allTimers.first?.warningTimeInSeconds, timer.warningTimeInSeconds)
+        XCTAssertEqual(timerModel[0].allTimers.first?.warningTimeInSeconds, 5)
+        XCTAssertEqual(timerModel[0][0].finalTimeInSeconds, timer.finalTimeInSeconds)
+        XCTAssertEqual(timerModel[0][0].finalTimeInSeconds, 2)
+        XCTAssertEqual(timer.group?.allTimers.first?.finalTimeInSeconds, 2)
+        XCTAssertEqual(timer.model?.allTimers.first?.finalTimeInSeconds, 2)
+        XCTAssertEqual(timer.group?.model?.allTimers.first?.finalTimeInSeconds, 2)
+        timerModel[0][0].finalTimeInSeconds = 3
+        XCTAssertEqual(timer.group?.model?.allTimers.first?.finalTimeInSeconds, 3)
         print("TimerModelTests.testCreateSimple (END)\n")
     }
 }
