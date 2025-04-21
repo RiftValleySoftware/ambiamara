d7e39925bbe9ddd8f223c199f6c46ce842180cfa
Chris Marshall <chris@riftvalleysoftware.com>
2025-04-20 20:48:15 -0400

    I have a bunch of stuff added to the sound selection popover, that came from the old app, but it doesn't work, yet.

--- Sources/RiVal.T/ViewControllers/RiValT_SoundSettings_ViewController.swift
+++ Sources/RiVal.T/ViewControllers/RiValT_SoundSettings_ViewController.swift
@@ -11,6 +11,7 @@
 import UIKit
 import RVS_Generic_Swift_Toolbox
 import RVS_UIKit_Toolbox
+import AVKit
 
 /* ###################################################################################################################################### */
 // MARK: - Special Bar Button for Sound Selection -
@@ -66,6 +67,116 @@ class RiValT_SoundSettings_ViewController: RiValT_Base_ViewController {
      */
     weak var group: TimerGroup?
     
+    /* ################################################################## */
+    /**
+     The size of the picker font
+    */
+    private static let _pickerFont = UIFont.boldSystemFont(ofSize: 20)
+
+    /* ################################################################## */
+    /**
+     The padding on either side of the labels we use as picker rows.
+    */
+    private static let _pickerPaddingInDisplayUnits = CGFloat(20)
+
+    /* ################################################################## */
+    /**
+     The SFSymbols names for the play sound button.
+    */
+    private static let _playSoundImageNames = ["speaker.wave.3.fill", "speaker.slash.fill"]
+    
+    /* ################################################################## */
+    /**
+     This is the audio player (for sampling sounds).
+    */
+    private var _audioPlayer: AVAudioPlayer!
+
+    /* ################################################################## */
+    /**
+     This aggregates our available sounds.
+     The sounds are files, stored in the resources, so this simply gets them, and stores them as path URIs.
+    */
+    private var _soundSelection: [String] = []
+    
+    /* ################################################################## */
+    /**
+     */
+    private var _selectedSoundIndex: Int = 0
+    
+    /* ################################################################## */
+    /**
+     */
+    private var _useVibrate: Bool = false
+
+    /* ################################################################## */
+    /**
+     If true, then the currently selected sound is playing.
+     This is set or cleared by the "play sound" button.
+    */
+    private var _isSoundPlaying: Bool = false {
+        didSet {
+            DispatchQueue.main.async {
+                self._audioPlayer?.stop()
+                self._audioPlayer = nil
+                
+                if self._isSoundPlaying,
+                   let url = URL(string: self._soundSelection[self._selectedSoundIndex]) {
+                    self.playThisSound(url)
+                }
+                
+                self.setUpForSoundPlayMode()
+            }
+        }
+    }
+    
+    /* ################################################################## */
+    /**
+     This references the presenting view controller.
+    */
+    weak var myController: RiValT_EditTimer_ViewController?
+    
+    /* ################################################################## */
+    /**
+     The segmented switch that controls the alarm mode.
+    */
+    @IBOutlet weak var alarmModeSegmentedSwitch: UISegmentedControl?
+
+    /* ################################################################## */
+    /**
+     The stach view that holds the vibrate switch.
+    */
+    @IBOutlet weak var vibrateSwitchStackView: UIView?
+
+    /* ################################################################## */
+    /**
+     The vibrate switch (only available on iPhones).
+    */
+    @IBOutlet weak var vibrateSwitch: UISwitch?
+    
+    /* ################################################################## */
+    /**
+     The label for the switch is actually a button, that toggles the switch.
+    */
+    @IBOutlet weak var vibrateSwitchLabelButton: UIButton?
+
+    /* ################################################################## */
+    /**
+     The picker view for the sounds. Only shown if the seg switch is set to sound.
+    */
+    @IBOutlet weak var soundsPickerView: UIPickerView?
+
+    /* ################################################################## */
+    /**
+     The stack view that holds the sound selection picker, and the play sound button.
+    */
+    @IBOutlet weak var soundSelectionStackView: UIView?
+    
+    /* ################################################################## */
+    /**
+     This is the "play sound" button.
+    */
+    @IBOutlet weak var soundPlayButton: UIButton?
+
     /* ############################################################## */
     /**
      Called when the view has loaded.
@@ -82,5 +193,173 @@ class RiValT_SoundSettings_ViewController: RiValT_Base_ViewController {
         } else {
             self.navigationItem.title = "SLUG-GROUP-SETTINGS".localizedVariant
         }
+    }
+    
+    /* ################################################################## */
+    /**
+     Sets the image to the play button, depending on whether or not the sound is playing.
+     It also starts or stops the sound play.
+    */
+    func setUpForSoundPlayMode() {
+        soundPlayButton?.setImage(UIImage(systemName: Self._playSoundImageNames[_isSoundPlaying ? 1 : 0]), for: .normal)
+    }
+
+    /* ################################################################## */
+    /**
+     This plays any sound, using a given URL.
+     
+     - parameter inSoundURL: This is the URI to the sound resource.
+     */
+    func playThisSound(_ inSoundURL: URL) {
+        if nil == _audioPlayer {
+            try? AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: []) // This line ensures that the sound will play, even with the ringer off.
+            if let audioPlayer = try? AVAudioPlayer(contentsOf: inSoundURL) {
+                audioPlayer.numberOfLoops = -1  // Keep repeating.
+                _audioPlayer = audioPlayer
+            }
+        }
+    
+        _audioPlayer?.play()
+    }
+}
+
+/* ###################################################################################################################################### */
+// MARK: Callbacks
+/* ###################################################################################################################################### */
+extension RiValT_SoundSettings_ViewController {
+    /* ################################################################## */
+    /**
+     Called when the alarm mode is changed.
+     
+     - parameter inSegmentedSwitch: The segmented switch that changed.
+    */
+    @IBAction func alarmModeSegmentedSwitchHit(_ inSegmentedSwitch: UISegmentedControl) {
+        self.selectionHaptic()
+//        RVS_AmbiaMara_Settings().alarmMode = 1 == inSegmentedSwitch.selectedSegmentIndex
+//        myController?.setAlarmIcon()
+        soundSelectionStackView?.isHidden = 1 != alarmModeSegmentedSwitch?.selectedSegmentIndex
+        _isSoundPlaying = false
+    }
+    
+    /* ################################################################## */
+    /**
+     Called when the vibrate switch, or its label, changes.
+     
+     - parameter inSender: The switch that changed, or the label button.
+    */
+    @IBAction func vibrateSwitchChanged(_ inSender: UIControl) {
+        if let vibrateSwitch = inSender as? UISwitch {
+            self.selectionHaptic()
+            self._useVibrate = vibrateSwitch.isOn
+        } else {
+            vibrateSwitch?.setOn(!(vibrateSwitch?.isOn ?? true), animated: true)
+            vibrateSwitch?.sendActions(for: .valueChanged)
+        }
+    }
+    
+    /* ################################################################## */
+    /**
+     Called when the "play sound" button is hit.
+     
+     - parameter: The button instance (ignored).
+    */
+    @IBAction func soundPlayButtonHit(_: UIButton) {
+        self.selectionHaptic()
+        _isSoundPlaying = !_isSoundPlaying
+    }
+}
+
+/* ###################################################################################################################################### */
+// MARK: UIPickerViewDataSource Conformance
+/* ###################################################################################################################################### */
+extension RiValT_SoundSettings_ViewController: UIPickerViewDataSource {
+    /* ################################################################## */
+    /**
+     - parameter in: The picker view (ignored).
+     
+     - returns the number of components (always 1)
+    */
+    func numberOfComponents(in: UIPickerView) -> Int { 1 }
+    
+    /* ################################################################## */
+    /**
+     - parameter: The picker view (ignored)
+     - parameter numberOfRowsInComponent: The 0-based index of the component we are querying.
+    */
+    func pickerView(_: UIPickerView, numberOfRowsInComponent inComponent: Int) -> Int { _soundSelection.count }
+}
+
+/* ###################################################################################################################################### */
+// MARK: UIPickerViewDelegate Conformance
+/* ###################################################################################################################################### */
+extension RiValT_SoundSettings_ViewController: UIPickerViewDelegate {
+    /* ################################################################## */
+    /**
+     This is called when a row is selected.
+     It verifies that the value is OK, and may change the selection, if not.
+     - parameter inPickerView: The picker instance.
+     - parameter didSelectRow: The 0-based row index, in the component.
+     - parameter inComponent: The component that contains the selected row (0-based index).
+    */
+    func pickerView(_ inPickerView: UIPickerView, didSelectRow inRow: Int, inComponent: Int) {
+        let wasPlaying = _isSoundPlaying
+        _isSoundPlaying = false
+        self._selectedSoundIndex = inRow
+        _isSoundPlaying = wasPlaying
+    }
+    
+    /* ################################################################## */
+    /**
+     This returns the view to display for the picker row.
+     
+     - parameter inPickerView: The picker instance.
+     - parameter viewForRow: The 0-based row index to be displayed.
+     - parameter forComponent: The 0-based component index for the row.
+     - parameter reusing: If a view will be reused, we'll use that, instead.
+     - returns: A new view, containing the row. If it is selected, it is displayed as reversed.
+    */
+    func pickerView(_ inPickerView: UIPickerView, viewForRow inRow: Int, forComponent inComponent: Int, reusing inView: UIView?) -> UIView {
+        guard let soundUri = URL(string: _soundSelection[inRow].urlEncodedString ?? "")?.lastPathComponent else { return UIView() }
+        let labelFrame = CGRect(origin: .zero, size: CGSize(width: inPickerView.bounds.size.width - Self._pickerPaddingInDisplayUnits, height: inPickerView.bounds.size.height / 3))
+        let label = UILabel(frame: labelFrame)
+        label.font = Self._pickerFont
+        label.textColor = .white
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.25
+        label.text = soundUri.localizedVariant
+        label.textAlignment = .center
+        
+        return label
+    }
+}
+
+/* ###################################################################################################################################### */
+// MARK: UIPickerViewAccessibilityDelegate Conformance
+/* ###################################################################################################################################### */
+extension RiValT_SoundSettings_ViewController: UIPickerViewAccessibilityDelegate {
+    /* ################################################################## */
+    /**
+     This returns the accessibility hint for the picker component.
+     
+     - parameter: The picker instance (ignored).
+     - parameter accessibilityHintForComponent: The 0-based component index for the label (ignored).
+     - returns: An accessibility string for the component.
+    */
+    func pickerView(_: UIPickerView, accessibilityHintForComponent: Int) -> String? { "SLUG-ACC-SOUND-PICKER".accessibilityLocalizedVariant }
+}
+
+/* ###################################################################################################################################### */
+// MARK: AVAudioPlayerDelegate Conformance
+/* ###################################################################################################################################### */
+extension RiValT_SoundSettings_ViewController: AVAudioPlayerDelegate {
+    /* ################################################################## */
+    /**
+     Called when the sound is done playing.
+     
+      - parameter: The player (ignored)
+      - parameter successfully: True, if the play was successful (also ignored).
+    */
+    func audioPlayerDidFinishPlaying(_: AVAudioPlayer, successfully: Bool) {
+        _isSoundPlaying = false
     }
 }
