ac8fc5246bea430043e7903ba852e2a6813b5380
Chris Marshall <chris@riftvalleysoftware.com>
2025-04-27 16:35:42 -0400

    OK. Looks like the basics are in place for the numerical running display, but I need to factor out a shared base class, and set up a numerical display class.

--- Sources/RiVal.T/ViewControllers/RunningTimerScreen/RiValT_RunningTimer_ViewController.swift
+++ Sources/RiVal.T/ViewControllers/RunningTimerScreen/RiValT_RunningTimer_ViewController.swift
@@ -9,9 +9,11 @@
  */
 
 import UIKit
+import AVKit
 import RVS_Generic_Swift_Toolbox
 import RVS_UIKit_Toolbox
 import RVS_RetroLEDDisplay
+import RVS_BasicGCDTimer
 
 /* ###################################################################################################################################### */
 // MARK: - The Main View Controller for the Running Timer -
@@ -30,6 +32,18 @@ class RiValT_RunningTimer_ViewController: RiValT_Base_ViewController {
      The repeat rate of the alarm "pulses."
      */
     private static let _alarmDurationInSeconds = TimeInterval(0.85)
+    
+    /* ############################################################## */
+    /**
+     The number of seconds to wait before the toolbar auto-hides.
+     */
+    private static let _autoHidePeriodInSeconds = TimeInterval(3)
+
+    /* ############################################################## */
+    /**
+     The period of the auto-hide duration.
+     */
+    private static let _autoHideAnimationDurationInSeconds = TimeInterval(0.5)
 
     /* ############################################################## */
     /**
@@ -42,6 +56,25 @@ class RiValT_RunningTimer_ViewController: RiValT_Base_ViewController {
      Used to fetch in a segue.
      */
     static let segueID = "run-timer"
+
+    /* ############################################################## */
+    /**
+     The timer that is used to trigger auto-hide (Toolbar On, Auto-Hide Working).
+     */
+    private var _autoHideTimer: RVS_BasicGCDTimer?
+
+    /* ################################################################## */
+    /**
+     This is the audio player (for playing alarm sounds).
+    */
+    private var _audioPlayer: AVAudioPlayer!
+    
+    /* ################################################################## */
+    /**
+     This aggregates our available sounds.
+     The sounds are files, stored in the resources, so this simply gets them, and stores them as path URIs.
+    */
+    private var _soundSelection: [String] = []
 
     /* ############################################################## */
     /**
@@ -147,6 +180,37 @@ class RiValT_RunningTimer_ViewController: RiValT_Base_ViewController {
      The stack view that contains the digit pairs.
      */
     @IBOutlet var digitContainerInternalView: UIView?
+    
+    /* ############################################################## */
+    /**
+     - returns: True, if the timer is currently running.
+     */
+    private var _isTimerRunning: Bool { false }
+
+    /* ############################################################## */
+    /**
+     - returns: True, if the current time is at the "starting gate."
+     */
+    private var _isAtStart: Bool { self.timer?.startingTimeInSeconds ?? 0 == self.timer?.currentTime ?? -1 }
+
+    /* ############################################################## */
+    /**
+     - returns: True, if the current time is at the end.
+     */
+    private var _isAtEnd: Bool { 0 == self.timer?.currentTime }
+
+    /* ############################################################## */
+    /**
+     - returns: True, if the current time is within the "warning" window.
+     */
+    private var _isWarning: Bool { false }
+
+    /* ############################################################## */
+    /**
+     - returns: True, if the current time is within the "final countdown" window.
+     */
+    private var _isFinal: Bool { false }
+    
 }
 
 /* ###################################################################################################################################### */
@@ -178,15 +242,6 @@ extension RiValT_RunningTimer_ViewController {
             self.blurFilterView?.isHidden = isHighContrastMode
         }
         self.hexGridImageView?.isHidden = isHighContrastMode
-        
-        let imageSize = self.hexGridImageView?.image?.size ?? .zero
-        if let bounds = self.digitContainerInternalView?.bounds,
-           imageSize != bounds.size {
-            DispatchQueue.global().async {
-                let image = self._generateHexOverlayImage(bounds)
-                DispatchQueue.main.async { self.hexGridImageView?.image = image }
-            }
-        }
     }
     
     /* ############################################################## */
@@ -214,10 +269,11 @@ extension RiValT_RunningTimer_ViewController {
         if let bounds = digitContainerInternalView?.bounds,
            imageSize != bounds.size {
             DispatchQueue.global().async {
-                let image = self._generateHexOverlayImage(bounds)
+                let image = Self._generateHexOverlayImage(bounds)
                 DispatchQueue.main.async { self.hexGridImageView?.image = image }
             }
         }
+        self.showToolbar()
     }
 
     /* ############################################################## */
@@ -229,64 +285,66 @@ extension RiValT_RunningTimer_ViewController {
     override func viewWillDisappear(_ inIsAnimated: Bool) {
         self.navigationController?.isNavigationBarHidden = false
         super.viewWillDisappear(inIsAnimated)
+        self._autoHideTimer?.invalidate()
+        self._autoHideTimer = nil
     }
     
-    /* ################################################################## */
-    /**
-     This returns a [CGMutablePath](https://developer.apple.com/documentation/coregraphics/cgmutablepath), describing a "pointy side up" hexagon.
-     
-     - parameter inHowBig: The radius, in display units.
-     
-     - returns: A [CGMutablePath](https://developer.apple.com/documentation/coregraphics/cgmutablepath), describing a "pointy side up" hexagon.
-     */
-    private class func _getHexPath(_ inHowBig: CGFloat) -> CGMutablePath {
-        /* ########################################################## */
-        /**
-         */
-        func _pointySideUpHexagon(_ inHowBig: CGFloat) -> [CGPoint] {
-            let angle = CGFloat(60).radians
-            let cx = CGFloat(inHowBig)  // x origin
-            let cy = CGFloat(inHowBig)  // y origin
-            let r = CGFloat(inHowBig)   // radius of circle
-            var points = [CGPoint]()
-            var minX: CGFloat = inHowBig * 2
-            var maxX: CGFloat = 0
-            for i in 0...6 {
-                let x = cx + r * cos(angle * CGFloat(i) - CGFloat(30).radians)
-                let y = cy + r * sin(angle * CGFloat(i) - CGFloat(30).radians)
-                minX = min(minX, x)
-                maxX = max(maxX, x)
-                points.append(CGPoint(x: x, y: y))
-            }
-            
-            for i in points.enumerated() {
-                points[i.offset] = CGPoint(x: i.element.x - minX, y: i.element.y)
-            }
-            
-            return points
-        }
-        
-        let path = CGMutablePath()
-        let points = _pointySideUpHexagon(inHowBig)
-        let cpg = points[0]
-        path.move(to: cpg)
-        points.forEach { path.addLine(to: $0) }
-        path.closeSubpath()
-        return path
-    }
-
     /* ################################################################## */
     /**
      This class generates an overlay image of a faint "hex grid" that allows us to simulate an old-fashioned "fluorescent" display.
      
      - parameter inBounds: The main bounds of the screen, from which the array will be calculated.
      */
-    private func _generateHexOverlayImage(_ inBounds: CGRect) -> UIImage? {
+    private static func _generateHexOverlayImage(_ inBounds: CGRect) -> UIImage? {
+        /* ############################################################## */
+        /**
+         This returns a [CGMutablePath](https://developer.apple.com/documentation/coregraphics/cgmutablepath), describing a "pointy side up" hexagon.
+         
+         - parameter inHowBig: The radius, in display units.
+         
+         - returns: A [CGMutablePath](https://developer.apple.com/documentation/coregraphics/cgmutablepath), describing a "pointy side up" hexagon.
+         */
+        func _getHexPath(_ inHowBig: CGFloat) -> CGMutablePath {
+            /* ########################################################## */
+            /**
+             */
+            func _pointySideUpHexagon(_ inHowBig: CGFloat) -> [CGPoint] {
+                let angle = CGFloat(60).radians
+                let cx = CGFloat(inHowBig)  // x origin
+                let cy = CGFloat(inHowBig)  // y origin
+                let r = CGFloat(inHowBig)   // radius of circle
+                var points = [CGPoint]()
+                var minX: CGFloat = inHowBig * 2
+                var maxX: CGFloat = 0
+                for i in 0...6 {
+                    let x = cx + r * cos(angle * CGFloat(i) - CGFloat(30).radians)
+                    let y = cy + r * sin(angle * CGFloat(i) - CGFloat(30).radians)
+                    minX = min(minX, x)
+                    maxX = max(maxX, x)
+                    points.append(CGPoint(x: x, y: y))
+                }
+                
+                for i in points.enumerated() {
+                    points[i.offset] = CGPoint(x: i.element.x - minX, y: i.element.y)
+                }
+                
+                return points
+            }
+            
+            let path = CGMutablePath()
+            let points = _pointySideUpHexagon(inHowBig)
+            let cpg = points[0]
+            path.move(to: cpg)
+            points.forEach { path.addLine(to: $0) }
+            path.closeSubpath()
+            return path
+        }
+
         let path = CGMutablePath()
         let sHexagonWidth = CGFloat(min(inBounds.size.width, inBounds.size.height) / 50)
         let radius: CGFloat = sHexagonWidth / 2
         
-        let hexPath: CGMutablePath = Self._getHexPath(radius)
+        let hexPath: CGMutablePath = _getHexPath(radius)
         let oneHexWidth = hexPath.boundingBox.size.width
         let oneHexHeight = hexPath.boundingBox.size.height
         
@@ -358,7 +416,7 @@ extension RiValT_RunningTimer_ViewController {
      This flashes the screen briefly cyan (pause)
      */
     func flashCyan() {
-        flasherView?.backgroundColor = UIColor(named: "Paused-Color")
+        self.flasherView?.backgroundColor = UIColor(named: "Paused-Color")
         self.impactHaptic(1.0)
         UIView.animate(withDuration: Self._flashDurationInSeconds,
                        animations: { [weak self] in
@@ -373,7 +431,7 @@ extension RiValT_RunningTimer_ViewController {
      This flashes the screen briefly green
      */
     func flashGreen() {
-        flasherView?.backgroundColor = UIColor(named: "Start-Color")
+        self.flasherView?.backgroundColor = UIColor(named: "Start-Color")
         self.impactHaptic()
         UIView.animate(withDuration: Self._flashDurationInSeconds,
                        animations: { [weak self] in
@@ -388,7 +446,7 @@ extension RiValT_RunningTimer_ViewController {
      This flashes the screen briefly yellow
      */
     func flashYellow() {
-        flasherView?.backgroundColor = UIColor(named: "Warn-Color")
+        self.flasherView?.backgroundColor = UIColor(named: "Warn-Color")
         self.impactHaptic()
         UIView.animate(withDuration: Self._flashDurationInSeconds,
                        animations: { [weak self] in
@@ -403,13 +461,144 @@ extension RiValT_RunningTimer_ViewController {
      This flashes the screen briefly red
      */
     func flashRed(_ inIsHard: Bool = false) {
-        impactHaptic(inIsHard ? 1.0 : 0.5)
-        flasherView?.backgroundColor = UIColor(named: "Final-Color")
+        self.impactHaptic(inIsHard ? 1.0 : 0.5)
+        self.flasherView?.backgroundColor = UIColor(named: "Final-Color")
         UIView.animate(withDuration: Self._flashDurationInSeconds,
                        animations: { [weak self] in
                                         self?.flasherView?.backgroundColor = .clear
                                     },
                        completion: nil
         )
+    }
+    
+    /* ############################################################## */
+    /**
+     This animates the toolbar into visibility.
+     */
+    func showToolbar() {
+        self._autoHideTimer?.invalidate()
+        self._autoHideTimer = nil
+
+        guard RiValT_Settings().displayToolbar,
+              RiValT_Settings().autoHideToolbar
+        else {
+            self.controlToolbar?.alpha = 1.0
+            return
+        }
+        
+        self._autoHideTimer = RVS_BasicGCDTimer(timeIntervalInSeconds: Self._autoHidePeriodInSeconds,
+                                                delegate: self,
+                                                leewayInMilliseconds: 100,
+                                                onlyFireOnce: true,
+                                                queue: .main, isWallTime: true)
+        self._autoHideTimer?.isRunning = true
+
+        if 1.0 > (self.controlToolbar?.alpha ?? 1) {
+            self.controlToolbar?.alpha = 0.0
+            view.layoutIfNeeded()
+            UIView.animate(withDuration: Self._autoHideAnimationDurationInSeconds,
+                           animations: { [weak self] in
+                                            self?.controlToolbar?.alpha = 1.0
+                                            self?.view.layoutIfNeeded()
+                                        },
+                           completion: nil
+            )
+        }
+    }
+    
+    /* ############################################################## */
+    /**
+     This animates the toolbar into invisibility.
+     */
+    func hideToolbar() {
+        self._autoHideTimer?.invalidate()
+        self._autoHideTimer = nil
+        self.controlToolbar?.alpha = 1.0
+        
+        guard RiValT_Settings().displayToolbar,
+              RiValT_Settings().autoHideToolbar
+        else { return }
+
+        view.layoutIfNeeded()
+        UIView.animate(withDuration: Self._autoHideAnimationDurationInSeconds,
+                       animations: { [weak self] in
+                                        self?.controlToolbar?.alpha = 0.0
+                                        self?.view.layoutIfNeeded()
+                                    },
+                       completion: nil
+        )
+    }
+    
+    /* ############################################################## */
+    /**
+     */
+    func rewindHit() {
+        
+    }
+    
+    /* ############################################################## */
+    /**
+     */
+    func stopHit() {
+        
+    }
+    
+    /* ############################################################## */
+    /**
+     */
+    func playPauseHit() {
+        
+    }
+
+    /* ############################################################## */
+    /**
+     */
+    func fastForwardHit() {
+        
+    }
+
+    /* ############################################################## */
+    /**
+     One of the toolbar controls was hit.
+     
+     - parameter inSender: The item that was activated.
+     */
+    @IBAction func toolbarItemHit(_ inSender: UIBarButtonItem) {
+        self.selectionHaptic()
+        if stopToolbarItem == inSender {
+            self.stopHit()
+        } else if rewindToolbarItem == inSender {
+            rewindHit()
+        } else if fastForwardBarButtonItem == inSender {
+            fastForwardHit()
+        } else if playPauseToolbarItem == inSender {
+            self.playPauseHit()
+        }
+        
+        self.showToolbar()
+    }
+}
+
+/* ###################################################################################################################################### */
+// MARK: RVS_BasicGCDTimerDelegate Conformance
+/* ###################################################################################################################################### */
+extension RiValT_RunningTimer_ViewController: RVS_BasicGCDTimerDelegate {
+    /* ############################################################## */
+    /**
+     Called when the timer fires.
+     
+     - parameter inTimer: The timer
+     */
+    func basicGCDTimerCallback(_ inTimer: RVS_BasicGCDTimer) {
+        DispatchQueue.main.async { [weak self] in
+            guard self?._autoHideTimer != inTimer else {
+                #if DEBUG
+                    print("Triggering the auto-hide timer")
+                #endif
+                self?.hideToolbar()
+
+                return
+            }
+        }
     }
 }
