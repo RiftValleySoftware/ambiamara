afdd3c4ef0ee43f9f749bd56121212ef22e34a5e
Chris Marshall <chris@riftvalleysoftware.com>
2025-05-09 15:56:21 -0400

    Bit more work, preparing the observable model.

--- Sources/WatchApp/RiValT_Watch_App.swift
+++ Sources/WatchApp/RiValT_Watch_App.swift
@@ -20,30 +20,44 @@ import WatchKit
 class ObservableModel: ObservableObject {
     /* ################################################################## */
     /**
+     This is the basic model for the whole app. It handles communication with the phone, as well as the local timer instance.
     */
     var wcSessionDelegateHandler: RiValT_WatchDelegate
     
     /* ################################################################## */
     /**
+     This is the external update handler that we'll call, when it's time to refresh. It is always called in the main thread.
+    */
+    var updateHandler: ((_: ObservableModel) -> Void)
+    
+    /* ################################################################## */
+    /**
+     This is an accessor for the local timer model object.
     */
     var timerModel: TimerModel? { self.wcSessionDelegateHandler.timerModel }
     
     /* ################################################################## */
     /**
+     This is a direct accessor for the local selected timer.
     */
     var currentTimer: Timer? { self.timerModel?.selectedTimer }
 
     /* ################################################################## */
     /**
+     This is a direct accessor for the group, to which the local selected timer belongs.
     */
     var currentGroup: TimerGroup? { self.currentTimer?.group }
 
     /* ################################################################## */
     /**
+     Default initializer.
+     
+     It creates the communication instance, and sets up the various local callbacks.
     */
-    init() {
+    init(updateHandler inUpdateHandler: @escaping (_: ObservableModel) -> Void) {
+        self.updateHandler = inUpdateHandler
         self.wcSessionDelegateHandler = RiValT_WatchDelegate()
-        self.wcSessionDelegateHandler.updateHandler = self.updateHandler
+        self.wcSessionDelegateHandler.updateHandler = self.delegateUpdateHandler
         self.wcSessionDelegateHandler.timerModel.selectedTimer?.tickHandler = self.tickHandler
         self.wcSessionDelegateHandler.timerModel.selectedTimer?.transitionHandler = self.transitionHandler
     }
@@ -54,7 +68,8 @@ class ObservableModel: ObservableObject {
      
      - parameter inWatchDelegate: The Watch communication instance.
     */
-    func updateHandler(_ inWatchDelegate: RiValT_WatchDelegate?) {
+    func delegateUpdateHandler(_ inWatchDelegate: RiValT_WatchDelegate?) {
+        self.updateHandler(self)
     }
     
     /* ################################################################## */
@@ -64,12 +79,14 @@ class ObservableModel: ObservableObject {
      - parameter inTimer: The timer instance that's "ticking."
     */
     func tickHandler(_ inTimer: Timer) {
+        DispatchQueue.main.async { self.updateHandler(self) }
     }
     
     /* ################################################################## */
     /**
     */
     func transitionHandler(_ inTimer: Timer, _ inFromState: TimerEngine.Mode, _ inToState: TimerEngine.Mode) {
+        DispatchQueue.main.async { self.updateHandler(self) }
     }
 }
 
@@ -83,9 +100,8 @@ class ObservableModel: ObservableObject {
 struct RiValT_Watch_App: App {
     /* ################################################################## */
     /**
-     This handles the session delegate.
      */
-    @State private var _wcSessionDelegateHandler: RiValT_WatchDelegate
+    private var _model: ObservableModel
 
     /* ################################################################## */
     /**
@@ -93,7 +109,8 @@ struct RiValT_Watch_App: App {
      */
     var body: some Scene {
         WindowGroup {
-            RiValT_Watch_App_MainContentView(wcSessionDelegateHandler: self.$_wcSessionDelegateHandler)
+            Text("HAI")
+//            RiValT_Watch_App_MainContentView(wcSessionDelegateHandler: self._model.wcSessionDelegateHandler)
         }
     }
     
@@ -102,33 +119,8 @@ struct RiValT_Watch_App: App {
      Default initializer
      */
     init() {
-        self._wcSessionDelegateHandler = RiValT_WatchDelegate()
-        self._wcSessionDelegateHandler.updateHandler = self.updateHandler
-    }
-    
-    /* ################################################################## */
-    /**
-     Called upon getting an update from the phone. Always called in the main thread.
-     
-     - parameter inWatchDelegate: The Watch communication instance.
-    */
-    func updateHandler(_ inWatchDelegate: RiValT_WatchDelegate?) {
-        self._wcSessionDelegateHandler.timerModel.selectedTimer?.tickHandler = self.tickHandler
-        self._wcSessionDelegateHandler.timerModel.selectedTimer?.transitionHandler = self.transitionHandler
-    }
-    
-    /* ################################################################## */
-    /**
-     Called for each "tick."
-     
-     - parameter inTimer: The timer instance that's "ticking."
-    */
-    func tickHandler(_ inTimer: Timer) {
-    }
-    
-    /* ################################################################## */
-    /**
-    */
-    func transitionHandler(_ inTimer: Timer, _ inFromState: TimerEngine.Mode, _ inToState: TimerEngine.Mode) {
+        self._model = ObservableModel { inModel in
+            print("Hello!")
+        }
     }
 }
